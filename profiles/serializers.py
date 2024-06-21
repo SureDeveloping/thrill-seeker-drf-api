3ee4a09636dde52c37d11d4c9eb1af375ef38e55
Stephen Sure <stephan.sure@gmail.com>
2024-06-21 09:04:27 +0000

    Add auth. to profiles app

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,11 +4,17 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.user
+
 
     class Meta:
         model = Profile
         fields = [
             'id', 'user', 'favorite_park', 'favorite_ride', 
             'userbio', 'created_at', 'updated_at', 
-            'profile_picture'
+            'profile_picture', 'is_owner',
         ]
