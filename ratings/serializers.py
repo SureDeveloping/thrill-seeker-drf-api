d739796f0b009c482903bd8652ea6852f1801b65
Stephen Sure <stephan.sure@gmail.com>
2024-06-24 09:00:08 +0000

    Add like filter to ratings serial.

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.db import IntegrityError
 from .models import Rating
+from likes.models import Like
 
 
 class RatingSerializer(serializers.ModelSerializer):
@@ -12,10 +13,20 @@ class RatingSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='user.profile.id')
     profile_picture = serializers.ReadOnlyField(
         source='user.profile.profile_picture.url')
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.user
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                user=user, rating=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Rating
@@ -23,7 +34,7 @@ class RatingSerializer(serializers.ModelSerializer):
             'id', 'user', 'is_owner', 'park', 'rating', 
             'last_visisted_at','explanation', 
             'profile_id', 'profile_picture',
-            'created_at', 'updated_at', 
+            'created_at', 'updated_at', 'like_id',
         ]
 
     def create(self, validated_data):
