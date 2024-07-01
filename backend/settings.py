9cdc42e2258e2bc20c993e9b25cdc1c8ce18182b
Stephen Sure <stephan.sure@gmail.com>
2024-07-01 15:15:58 +0000

    Change client origin dev

--- backend/settings.py
+++ backend/settings.py
@@ -132,10 +132,7 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.codeinstitute-ide\.net$",
-    ]
+CORS_ALLOWED_ORIGIN = os.environ.get('CLIENT_ORIGIN_DEV', '')
 
 #url need to change depending on the gitpod url.
 
