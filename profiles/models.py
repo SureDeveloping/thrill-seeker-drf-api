24da4cea163856dfeb84c214a7dd3c6ceeeb6f9a
Stephen Sure <stephan.sure@gmail.com>
2024-07-14 09:31:29 +0000

    Add auto profile creation

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,5 +15,16 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+            ordering = ['-created_at']
+
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f"{self.user.username}'s Profile"
+ 
+     
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+
+    post_save.connect(create_profile, sender=User)
