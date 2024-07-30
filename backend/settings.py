6b2d9cbbce8cba72f48d2af534f8ce2d4d9b72e3
Stephen Sure <stephan.sure@gmail.com>
2024-07-30 10:24:08 +0000

    Delete email send function

--- backend/settings.py
+++ backend/settings.py
@@ -214,12 +214,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
