ba6a879611b1e7aa5e043cc97bbd9599d12e8fc8
Stephen Sure <stephan.sure@gmail.com>
2024-07-30 12:33:29 +0000

    Delete email send to admin email adress

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import EmailValidator
-from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
@@ -40,21 +39,3 @@ class ContactForm(models.Model):
 
     def __str__(self):
         return f"Message send by {self.first_name} {self.last_name}"
-
-
-@receiver(post_save, sender=ContactForm)
-def send_contact_email(sender, instance, created, **kwargs):
-    if created:
-        subject = instance.subject
-        message = (
-            f"You have received a new message from the contact form:\n\n"
-            f"Name: {instance.first_name} {instance.last_name}\n"
-            f"Email: {instance.email}\n"
-            f"Subject: {instance.subject}\n\n"
-            f"Message:\n{instance.message}\n"
-            f"Created at: {instance.created_at}\n"
-        )
-        from_email = instance.email
-        to_email = [settings.ADMIN_EMAIL]
-
-        send_mail(subject, message, from_email, to_email)
--- contact/serializers.py
+++ contact/serializers.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 from .models import ContactForm
-import os
-from django.core.mail import send_mail
 
-ADMIN_EMAIL = os.environ.get("EMAIL_ADDRESS")
 
 class ContactFormSerializer(serializers.ModelSerializer):
     """
@@ -43,26 +40,3 @@ class ContactFormSerializer(serializers.ModelSerializer):
             "error_messages": {"blank": "This field is required"}
         },
     }
-
-    def create(self, validated_data):
-        """
-        Creates contact form and send it via email to the admin.
-        """
-        contact = ContactForm.objects.create(**validated_data)
-
-        # Send email
-        subject = f"New Contact Form Submission: {contact.subject}"
-        message = (
-            f"First Name: {contact.first_name}\n"
-            f"Last Name: {contact.last_name}\n"
-            f"Email: {contact.email}\n"
-            f"Subject: {contact.subject}\n"
-            f"Message:\n{contact.message}\n"
-            f"Created at: {contact.created_at}\n"
-        )
-
-        try:
-            send_mail(subject, message, contact.email, [ADMIN_EMAIL])
-        except Exception as e:
-            print(f"Failed to send Contact Form, Please try again {e}")
-        return contact
