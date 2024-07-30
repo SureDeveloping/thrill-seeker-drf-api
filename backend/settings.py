bbf8211153ae6fa696f1c269507424b43fd5f7d5
Stephen Sure <stephan.sure@gmail.com>
2024-07-30 23:30:14 +0000

    Change debug to false

--- backend/settings.py
+++ backend/settings.py
@@ -63,8 +63,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEV' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
